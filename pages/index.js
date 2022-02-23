e64dbd69c57fb9277d67ce1682830df9eb0687f7
Maya Kano <mayakanoweb@gmail.com>
2022-02-23 11:32:51 -0500

    fetched the product data from shopify store

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,17 @@
-import Head from 'next/head'
+// import Head from 'next/head'
+import { getProductsInCollection } from '../lib/shopify'
 
-export default function Home() {
+//using destructuring for 'properties' so we don't need to use the word props
+export default function Home({ products }) {
+  console.log(products)
+
   return <div className="text-3xl">Hello from Shopify Next.js</div>
 }
+
+export async function getStaticProps() {
+  const products = await getProductsInCollection()
+  return {
+    props: { products }, // will be passed to the page component as props
+  }
+}
+//get static function comes with NextJs
