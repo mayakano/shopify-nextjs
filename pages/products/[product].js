5bb504bc7e7cabeb49bb8cf6728f15b5c711b310
Maya Kano <mayakanoweb@gmail.com>
2022-02-23 13:22:02 -0500

    fetching more data from products to display

---
+++ pages/products/[product].js
@@ -0,0 +1,32 @@
+import { getAllProducts, getProduct } from '../../lib/shopify'
+
+export default function ProductPage({ product }) {
+  return <div>{product.title}</div>
+}
+
+export async function getStaticPaths() {
+  const products = await getAllProducts()
+  //from the products we're getting the paths
+  const paths = products.map((item) => {
+    const handle = String(item.node.handle)
+
+    //passing product key with the handle value
+    return {
+      params: { product: handle },
+    }
+  })
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const product = await getProduct(params.product)
+
+  return {
+    props: {
+      product,
+    },
+  }
+}
