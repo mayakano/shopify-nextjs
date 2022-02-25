707fbea40310725588056945a1ca4292b8efe04e
Maya Kano <mayakanoweb@gmail.com>
2022-02-25 01:24:38 -0500

    added seo tags

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,37 @@
 import { getProductsInCollection } from '../lib/shopify'
 import ProductList from '../components/ProductList'
 import Hero from '../components/Hero'
+import Head from 'next/head'
 
 //using destructuring for 'properties' so we don't need to use the word props
 export default function Home({ products }) {
   return (
     <div>
+      <Head>
+        <title>NextGraph</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta
+          http-equiv="Content-Type"
+          content="text/html; charset=ISO-8859-1"
+        ></meta>
+        <meta
+          name="description"
+          content="Modern Headless Shopify E-Commerce Website"
+        />
+        <meta property="og:title" content="NextGraph" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://next-graph.vercel.app/" />
+        <meta
+          property="og:image"
+          content="https://next-graph.vercel.app/nextgraph.png"
+        />
+        <meta
+          property="og:description"
+          content="Modern Headless Shopify E-Commerce Website"
+        />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:site_name" content="NextGraph" />
+      </Head>
       <Hero />
       <ProductList products={products} />
     </div>
